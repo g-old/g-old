22a528186395f487f454aae94d7fd4ee2967642d
Sim <thenambawan@gmail.com>
2017-02-16 23:30:17 +0100

    adjustd votes seeding algorithm to seed with unique votes

--- src/data/db/seeds/06votes.js
+++ src/data/db/seeds/06votes.js
@@ -4,39 +4,74 @@
 function random(array) {
   return array[Math.floor(array.length * Math.random())];
 }
-
+function randomNumber(max) {
+  return Math.floor(max * Math.random());
+}
 
 /* eslint comma-dangle: ["error", {"functions": "never"}]*/
 exports.seed = function (knex, Promise) {
-  let records = 200;
-  const ar = [];
-  while (records) { // TODO move to new Array(x) as soon as it is supported
-    ar.push(1);
-    records -= 1;
-  }
   let time;
 
-// TODO This fn is wrong as it can insert multiple votes for the same proposal
-  function seeding() {
-    return Promise.resolve([
-      'users',
-      'proposals'])
-  .map((table) => knex.select('id').from(table).pluck('id'))
-  .spread((users, proposals) => ar.map(() => {
-    time = new Date();
-    return {
-      user_id: random(users),
-      proposal_id: random(proposals),
-      position: Math.random() < 0.5 ? 'pro' : 'con',
-      created_at: time,
-      updated_at: time
-    };
+  function getUsers() {
+    return new Promise((resolve) => {
+      knex.select('id').from('users').pluck('id').then((data) => resolve(data));
+    });
   }
-)
-)
-.then((properties) => knex.into('votes').insert(properties)
+  function getProposals() {
+    return new Promise((resolve) => {
+      knex.select('id').from('proposals').pluck('id').then((data) => resolve(data));
+    });
+  }
+  function getUniqueIDs(proposals, num) {
+    const ids = [];
+    while (ids.length < num) {
+      const id = random(proposals);
+      if (ids.indexOf(id) === -1) {
+        ids.push(id);
+      }
+    }
+    return ids;
+  }
+  function seeding() {
+    return new Promise((resolve) => getUsers()
+  .then((users) => {
+    getProposals().then((proposals) =>
+      new Promise((resolv) => {
+        const results = [];
+        const updates = [];
+        users.map((id) => {
+          const num = randomNumber(proposals.length);
+          const ids = getUniqueIDs(proposals, num);
+          for (let i = 0; i < num; i += 1) {
+            time = new Date();
+            const proposalID = ids[i];
+            const data = {
+              user_id: id,
+              proposal_id: proposalID,
+              position: Math.random() < 0.5 ? 'pro' : 'con',
+              created_at: time,
+              updated_at: time
+            };
+            updates.push(knex('proposals')
+            .where({ id: proposalID })
+            .increment('votes', 1));
+            results.push(data);
+          }
+          return null;
+        });
+
+        Promise.all(updates).then(() => {
+          resolv(results);
+        });
+      })
+    )
+    .then((properties) => knex.into('votes').insert(properties))
+    .then(() => resolve());
+  })
   );
   }
+
+
   return knex
   .raw('ALTER TABLE votes DISABLE TRIGGER ALL;')
   .then(() => knex('votes').del())
