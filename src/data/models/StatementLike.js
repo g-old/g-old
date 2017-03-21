81057f4999cf070ee94bfd8cb748c03eabab5eef
Sim <thenambawan@gmail.com>
2017-03-21 21:09:41 +0100

    fixed race condition in votes and likes table, added unique key constraints

--- src/data/models/StatementLike.js
+++ src/data/models/StatementLike.js
@@ -1,9 +1,9 @@
 import knex from '../knex';
 // eslint-disable-next-line no-unused-vars
-function checkCanSee(viewer, data) { // TODO change data returned based on permissions
+function checkCanSee(viewer, data) {
+  // TODO change data returned based on permissions
   return true;
 }
-
 
 class StatementLike {
   constructor(data) {
@@ -20,52 +20,71 @@ class StatementLike {
 
   static async delete(viewer, data) {
     if (!data.id || !viewer) return null;
-      // eslint-disable-next-line prefer-arrow-callback
+    // eslint-disable-next-line prefer-arrow-callback
     const deletedLike = await knex.transaction(async function (trx) {
-      const likeInDB = await knex('statement_likes').where({ id: data.id, user_id: viewer.id }).select();
+      const likeInDB = await knex('statement_likes')
+        .transacting(trx)
+        .forUpdate()
+        .where({ id: data.id, user_id: viewer.id })
+        .select();
       if (likeInDB.length === 0) throw Error('Nothing to delete!');
-      await trx.where({ id: data.id, user_id: viewer.id })
-        .into('statement_likes')
+      await knex('statement_likes')
+        .transacting(trx)
+        .forUpdate()
+        .where({ id: data.id, user_id: viewer.id })
         .del();
       // update votecount;
-      await trx.where({ id: data.statementId }).decrement('likes', 1).into('statements');
+      await knex('statements')
+        .transacting(trx)
+        .forUpdate()
+        .where({ id: data.statementId })
+        .decrement('likes', 1);
       return likeInDB;
     });
-    console.log(deletedLike);
-    console.log('deletetdLIKE');
+
     const delLike = new StatementLike(deletedLike[0]);
-    console.log(delLike);
+
     return delLike || null;
   }
 
   static async create(viewer, data, loaders) {
-  // validate
+    // validate
     if (!data.statementId || !viewer) return null;
 
-  // create
-  // eslint-disable-next-line prefer-arrow-callback
+    // create
+    // eslint-disable-next-line prefer-arrow-callback
     const newLikeId = await knex.transaction(async function (trx) {
-      const likeInDB = await knex('statement_likes').where({ statement_id: data.statementId, user_id: viewer.id }).pluck('id');
-      if (likeInDB.length !== 0) throw Error('Already voted!');
-      const id = await trx
-      .insert({
-        user_id: viewer.id,
-        statement_id: data.statementId,
-        created_at: new Date() }, 'id')
+      const likeInDB = await knex('statement_likes')
+        .transacting(trx)
+        .forUpdate()
+        .where({ statement_id: data.statementId, user_id: viewer.id })
+        .pluck('id')
         .into('statement_likes');
+      if (likeInDB.length !== 0) throw Error('Already liked!');
+      const id = await knex('statement_likes').transacting(trx).forUpdate().insert(
+        {
+          user_id: viewer.id,
+          statement_id: data.statementId,
+          created_at: new Date(),
+        },
+        'id',
+      );
 
       if (id.length === 0) throw Error('No Id returned');
 
       // update votecount;
       // TODO these updates are sequential - can db-ops be parallel in transactions?
-      await trx.where({ id: data.statementId }).increment('likes', 1).into('statements');
+      await knex('statements')
+        .transacting(trx)
+        .forUpdate()
+        .where({ id: data.statementId })
+        .increment('likes', 1)
+        .into('statements');
       return id[0];
     });
     if (!newLikeId) return null;
     return StatementLike.gen(viewer, newLikeId, loaders);
   }
-
 }
-
 
 export default StatementLike;
--- src/data/models/Vote.js
+++ src/data/models/Vote.js
@@ -1,10 +1,10 @@
 import knex from '../knex';
 import Poll from './Poll';
 // eslint-disable-next-line no-unused-vars
-function checkCanSee(viewer, data) { // TODO change data returned based on permissions
+function checkCanSee(viewer, data) {
+  // TODO change data returned based on permissions
   return true;
 }
-
 
 class Vote {
   constructor(data) {
@@ -22,9 +22,13 @@ class Vote {
     return new Vote(data);
   }
 
-  static async validate(viewer, data, loaders, poll, mutation = false) {
+  static async validate(viewer, data, loaders, poll, trx, mutation = false) {
     if (!poll || !poll.isVotable()) return null;
-    const voteInDB = await knex('votes').where({ poll_id: data.pollId, user_id: viewer.id }).select();
+    const voteInDB = await knex('votes')
+      .transacting(trx)
+      .forUpdate()
+      .where({ poll_id: data.pollId, user_id: viewer.id })
+      .select();
     if (mutation && voteInDB.length === 0) return null;
     if (voteInDB.length !== 0) return mutation ? { ...voteInDB[0] } : null;
     return true;
@@ -34,59 +38,69 @@ class Vote {
     if (!data.id) return null;
     if (!data.pollId) return null;
     const poll = await Poll.gen(viewer, data.pollId, loaders); // auth should happen here ...
-    const oldVote = await Vote.validate(viewer, data, loaders, poll, true);
 
-    if (!oldVote || !oldVote.position) return null;
-    // eslint-disable-next-line eqeqeq
-    if (data.id != oldVote.id) return null;
-    const newPosition = data.position === 1 ? 'pro' : 'con'; // dangerous?
-    if (newPosition !== oldVote.position) return null;
+    const deletedVote = await knex.transaction(async trx => {
+      const oldVote = await Vote.validate(viewer, data, loaders, poll, trx, true);
+      if (!oldVote || !oldVote.position) throw Error('Request invalid!');
+      // eslint-disable-next-line eqeqeq
+      if (data.id != oldVote.id) throw Error('Request invalid!');
+      const newPosition = data.position === 1 ? 'pro' : 'con'; // dangerous?
+      if (newPosition !== oldVote.position) throw Error('Request invalid!');
+      // eslint-disable-next-line newline-per-chained-call
+      await knex('votes').transacting(trx).forUpdate().where({ id: data.id }).del();
 
-    // eslint-disable-next-line prefer-arrow-callback
-    await knex.transaction(async function (trx) {
-      await trx.where({ id: data.id })
-          .into('votes')
-          .del();
-
-        // update votecount
+      // update votecount
       const columns = ['upvotes', 'downvotes'];
       const index = newPosition === 'con' ? 1 : 0;
-      await trx.where({ id: data.pollId }).decrement(columns[index], 1).into('polls');
-      await trx.where({ id: data.pollId }).decrement('num_voter', 1).into('polls');
+      await knex('polls')
+        .transacting(trx)
+        .forUpdate()
+        .where({ id: data.pollId })
+        .decrement(columns[index], 1);
+      await knex('polls')
+        .transacting(trx)
+        .forUpdate()
+        .where({ id: data.pollId })
+        .decrement('num_voter', 1);
+      return oldVote;
     });
-    return new Vote(oldVote) || null;
+    return new Vote(deletedVote) || null;
   }
 
   static async update(viewer, data, loaders) {
     if (!data.id) return null;
     if (!data.pollId) return null;
     const poll = await Poll.gen(viewer, data.pollId, loaders); // auth should happen here ...
-    const oldVote = await Vote.validate(viewer, data, loaders, poll, true);
-    if (!oldVote || !oldVote.position) return null;
-    // eslint-disable-next-line eqeqeq
-    if (data.id != oldVote.id) return null;
     if (await poll.isUnipolar(viewer, loaders)) {
       return null; // delete is the right method
     }
-    const newPosition = data.position === 1 ? 'pro' : 'con'; // dangerous?
-    if (newPosition === oldVote.position) return null;
+    await knex.transaction(async trx => {
+      const oldVote = await Vote.validate(viewer, data, loaders, poll, trx, true);
+      if (!oldVote || !oldVote.position) throw Error('Position mismatch');
+      // eslint-disable-next-line eqeqeq
+      if (data.id != oldVote.id) throw Error('Id mismatch');
 
-    // update
-    // eslint-disable-next-line prefer-arrow-callback
-    await knex.transaction(async function (trx) {
-      await trx.where({ id: data.id })
-      .update({
+      const newPosition = data.position === 1 ? 'pro' : 'con'; // dangerous?
+      if (newPosition === oldVote.position) throw Error('Request invalid');
+      // eslint-disable-next-line newline-per-chained-call
+      await knex('votes').transacting(trx).forUpdate().where({ id: data.id }).update({
         position: newPosition,
-        updated_at: new Date() })
-        .into('votes');
-
-        // update votecount
+        updated_at: new Date(),
+      });
+      // update votecount
       const columns = ['upvotes', 'downvotes'];
       let index = newPosition === 'con' ? 1 : 0;
-      await trx.where({ id: data.pollId }).increment(columns[index], 1).into('polls');
-      await trx.where({ id: data.pollId }).decrement(columns[index = 1 - index], 1).into('polls');
+      await knex('polls')
+        .transacting(trx)
+        .forUpdate()
+        .where({ id: data.pollId })
+        .increment(columns[index], 1);
+      await knex('polls')
+        .transacting(trx)
+        .forUpdate()
+        .where({ id: data.pollId })
+        .decrement(columns[(index = 1 - index)], 1);
     });
-
     return Vote.gen(viewer, data.id, loaders);
   }
 
@@ -103,37 +117,42 @@ class Vote {
     if (await poll.isUnipolar(viewer, loaders)) {
       position = 'pro';
     }
-
-    // create
-    // eslint-disable-next-line prefer-arrow-callback
-    const newVoteId = await knex.transaction(async function (trx) {
-      const voteInDB = await knex('votes').where({ poll_id: data.pollId, user_id: viewer.id }).pluck('id');
-      // To avoid multiple votings, we have to place the check in the transaction
-      // and lock the table if not locked  (how?)
+    const newVoteId = await knex.transaction(async trx => {
+      const voteInDB = await knex('votes')
+        .transacting(trx)
+        .forUpdate()
+        .where({ poll_id: data.pollId, user_id: viewer.id })
+        .pluck('id');
       if (voteInDB.length !== 0) throw Error('Already voted!');
-      const id = await trx
-      .insert({
-        user_id: viewer.id,
-        poll_id: data.pollId,
-        position,
-        created_at: new Date() }, 'id')
-        .into('votes');
-
+      const id = await knex('votes').transacting(trx).forUpdate().insert(
+        {
+          user_id: viewer.id,
+          poll_id: data.pollId,
+          position,
+          created_at: new Date(),
+        },
+        'id',
+      );
       if (id.length === 0) throw Error('No Id returned');
-
-        // update votecount;
-        // TODO these updates are sequential - can db-ops be parallel in transactions?
+      // update votecount;
+      // TODO these updates are sequential - can db-ops be parallel in transactions?
       const columns = ['upvotes', 'downvotes'];
       const index = position === 'con' ? 1 : 0;
-      await trx.where({ id: data.pollId }).increment(columns[index], 1).into('polls');
-      await trx.where({ id: data.pollId }).increment('num_voter', 1).into('polls');
+      await knex('polls')
+        .transacting(trx)
+        .forUpdate()
+        .where({ id: data.pollId })
+        .increment(columns[index], 1);
+      await knex('polls')
+        .transacting(trx)
+        .forUpdate()
+        .where({ id: data.pollId })
+        .increment('num_voter', 1);
       return id[0];
     });
-
     if (!newVoteId) return null;
     return Vote.gen(viewer, newVoteId, loaders);
   }
-
 }
 
 export default Vote;
