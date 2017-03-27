1da2276823768324691ed480fec3b096ac6bd6a8
Sim <thenambawan@gmail.com>
2017-03-27 21:41:10 +0200

    deleted not existing import in reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,10 @@ import user from './user';
 import runtime from './runtime';
 import intl from './intl';
 import entities from './entities';
-import ui from './ui';
 
 export default combineReducers({
   user,
   runtime,
   intl,
   entities,
-  ui,
 });
