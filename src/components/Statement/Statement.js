e36084dc08a2265493dfd07b4424f6c42b8a4010
tobiasmayr89 <tobiasmayr89@gmail.com>
2017-04-05 22:41:59 +0200

    empty statement input mask is now much less prominent

--- src/components/Statement/Statement.js
+++ src/components/Statement/Statement.js
@@ -96,62 +96,75 @@ class Statement extends React.Component {
   }
 
   render() {
-    const minInputLen = this.state.textArea.val.length >= 3;
+    const isEmpty = this.state.textArea.val.length === 0;
+    const hasMinimumInput = this.state.textArea.val.length >= 5;
+    const inactive = this.props.asInput && isEmpty;
+    const canLike = this.props.user.role !== 'guest' &&
+      !this.props.asInput &&
+      !this.props.ownStatement;
+
     return (
-      <div className={cn(s.root, this.props.data.vote.position === 'pro' ? s.pro : s.contra)}>
-        <img
-          className={cn(s.avatar)}
-          src={
-            `https://api.adorable.io/avatars/256/${this.props.data.author.name}${this.props.data.author.surname}.io.png`
-          }
-          alt="IMG"
-        />
+      <div
+        className={cn(
+          s.root,
+          this.props.data.vote.position === 'pro' ? s.pro : s.contra,
+          inactive && s.inactive,
+        )}
+      >
+        {!inactive &&
+          <img
+            className={cn(s.avatar)}
+            src={
+              `https://api.adorable.io/avatars/256/${this.props.data.author.name}${this.props.data.author.surname}.io.png`
+            }
+            alt="IMG"
+          />}
         <div style={{ width: '100%' }}>
-          <div className={s.header}>
-            <div>
-              <span className={s.author}>
-                {this.props.data.author.name} {this.props.data.author.surname}
+          {!inactive &&
+            <div className={s.header}>
+              <div>
+                <span className={s.author}>
+                  {this.props.data.author.name} {this.props.data.author.surname}
+                </span>
+                <span>
+                  {this.props.data.likes ? ` (+${this.props.data.likes})` : ''}
+                  {canLike &&
+                    <button
+                      onClick={e => this.handleLikeClick(e, this.props.ownLike)}
+                      className={cn(
+                        s.like,
+                        this.props.ownLike ? 'fa fa-thumbs-up' : 'fa fa-thumbs-o-up',
+                      )}
+                    />}
+                </span>
+              </div>
+              <span className={s.menu}>
+                {(this.props.asInput ||
+                  this.props.ownStatement ||
+                  ['admin', 'mod'].includes(this.props.user.role)) &&
+                  <span style={{ marginRight: '0.5em' }}>
+                    {this.state.edit
+                      ? <span>
+                        <button onClick={this.onTextSubmit} disabled={!hasMinimumInput}>
+                          <i className="fa fa-check" />
+                        </button>
+                        <button
+                          onClick={this.onEndEditing}
+                          disabled={this.props.asInput && !hasMinimumInput}
+                        >
+                          <i className="fa fa-times" />
+                        </button>
+                      </span>
+                      : <button onClick={this.onEditStatement}>
+                        <i className="fa fa-pencil" />
+                      </button>}
+                    {!this.props.asInput &&
+                      <button onClick={this.onDeleteStatement}>
+                        <i className="fa fa-trash" />
+                      </button>}
+                  </span>}
               </span>
-              <span>
-                {this.props.data.likes ? ` (+${this.props.data.likes})` : ''}
-                {this.props.user.role !== 'guest' &&
-                  !this.props.asInput &&
-                  <button
-                    onClick={e => this.handleLikeClick(e, this.props.ownLike)}
-                    className={cn(
-                      s.like,
-                      this.props.ownLike ? 'fa fa-thumbs-up' : 'fa fa-thumbs-o-up',
-                    )}
-                  />}
-              </span>
-            </div>
-            <span className={s.menu}>
-              {(this.props.asInput ||
-                this.props.ownStatement ||
-                ['admin', 'mod'].includes(this.props.user.role)) &&
-                <span style={{ marginRight: '0.5em' }}>
-                  {this.state.edit
-                    ? <span>
-                      <button onClick={this.onTextSubmit} disabled={!minInputLen}>
-                        <i className="fa fa-check" />
-                      </button>
-                      <button
-                        onClick={this.onEndEditing}
-                        disabled={this.props.asInput && !minInputLen}
-                      >
-                        <i className="fa fa-times" />
-                      </button>
-                    </span>
-                    : <button onClick={this.onEditStatement}>
-                      <i className="fa fa-pencil" />
-                    </button>}
-                  {!this.props.asInput &&
-                    <button onClick={this.onDeleteStatement}>
-                      <i className="fa fa-trash" />
-                    </button>}
-                </span>}
-            </span>
-          </div>
+            </div>}
           <div className={s.text}>
             {this.state.edit
               ? <div>
