fefceb5da26cbde1f876439e99e9c68748e61c2d
tobiasmayr89 <tobiasmayr89@gmail.com>
2018-03-19 23:23:19 +0100

    initial work for dotenv: extending the config.js file

--- src/config.js
+++ src/config.js
@@ -15,12 +15,15 @@ if (process.env.BROWSER) {
   );
 }
 
+/** parse dotenv files first! */
+require('dotenv-extended').config();
+
 module.exports = {
   // default locale is the first one
   locales: ['de-DE', 'it-IT', 'lld-IT'],
 
   // Node.js app
-  port: process.env.PORT || 3000,
+  port: process.env.PORT,
 
   profiling: true,
 
@@ -30,7 +33,59 @@ module.exports = {
     clientUrl: process.env.API_CLIENT_URL || '',
     // API URL to be used in the server-side code
     serverUrl:
-      process.env.API_SERVER_URL ||
-      `http://localhost:${process.env.PORT || 3000}`,
+      process.env.API_SERVER_URL || `http://localhost:${process.env.PORT}`,
+  },
+
+  dbConfig: process.env.DATABASE_URL,
+  mailer: {
+    config: {
+      jsonTransport: process.env.MAILER_JSONTRANSPORT, // stream output to console
+      // https://nodemailer.com/smtp/
+      // gmail example, no pooling used!
+      host: process.env.MAILER_HOST,
+      port: process.env.MAILER_PORT,
+      secure: process.env.MAILER_SECURE, // upgrade later with STARTTLS
+      auth: {
+        user: process.env.MAILER_USER,
+        pass: process.env.MAILER_PASSWORD,
+      },
+    },
+    sender: process.env.MAILER_SENDER,
+  },
+  SENDGRID_API_KEY: 'api key', // or set it as env variable
+  recaptcha: {
+    secret: process.env.RECAPTCHA_SECRET, // dev keys
+    siteKey: process.env.RECAPTCHA_KEY,
+  },
+  cloudinary: {
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_secret: process.env.CLOUDINARY_SECRET,
+    api_key: process.env.CLOUDINARY_KEY,
+  },
+  // in gold folder open node REPL, type: var wp = require('web-push');wp.generateVAPIDKeys()
+  webpush: {
+    mail: process.env.WEBPUSH_MAIL,
+    publicKey: process.env.WEBPUSH_PUBKEY,
+    privateKey: process.env.WEBPUSH_PRIVKEY,
+  },
+
+  /** test-configuration */
+  test: {
+    dbConfig: process.env.TEST_DATABASE_URL,
+    mailer: {
+      config: {
+        jsonTransport: process.env.TEST_MAILER_JSONTRANSPORT, // stream output to console
+        // https://nodemailer.com/smtp/
+        // gmail example, no pooling used!
+        host: process.env.TEST_MAILER_HOST,
+        port: process.env.TEST_MAILER_PORT,
+        secure: process.env.TEST_MAILER_SECURE, // upgrade later with STARTTLS
+        auth: {
+          user: process.env.TEST_MAILER_USER,
+          pass: process.env.TEST_MAILER_PASSWORD,
+        },
+      },
+      sender: process.env.TEST_MAILER_SENDER,
+    },
   },
 };
