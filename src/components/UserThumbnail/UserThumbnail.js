2ab3f933f446b3564cda9eb7ea305a600cf08653
Sim <thenambawan@gmail.com>
2018-04-12 18:06:59 +0200

    added UserThumbnail component

---
+++ src/components/UserThumbnail/UserThumbnail.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './UserThumbnail.css';
+import Link from '../Link';
+
+const UserThumbnail = ({ user, label }) => {
+  const fullName = `${user.name} ${user.surname}`;
+  return user ? (
+    // eslint-disable-next-line jsx-a11y/anchor-is-valid
+    <Link to={`/accounts/${user.id}`} className={s.root}>
+      <img src={user.thumbnail} alt={fullName} />
+      {label}
+      <span>{fullName}</span>
+    </Link>
+  ) : (
+    label
+  );
+};
+
+UserThumbnail.propTypes = {
+  user: PropTypes.shape({}).isRequired,
+  label: PropTypes.string,
+};
+UserThumbnail.defaultProps = {
+  label: null,
+};
+
+export default withStyles(s)(UserThumbnail);
