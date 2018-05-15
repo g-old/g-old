70dbba58af597d73ec66585599aa027dc630868a
Sim <thenambawan@gmail.com>
2018-05-16 00:24:09 +0200

    added message migration

---
+++ src/data/db/migrations/20180515224322_change_message.js
@@ -0,0 +1,30 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('messages', table => {
+      table
+        .json('to')
+        .notNullable()
+        .defaultsTo('[]');
+      table.enu('receiver_type', ['group', 'user']);
+      table.text('msg_html');
+      table.renameColumn('title', 'subject');
+      table.dropColumn('location');
+      table.dropColumn('date');
+      table.dropColumn('type');
+    }),
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('messages', table => {
+      table.dropColumn('to');
+      table.dropColumn('receiver_type');
+      table.dropColumn('msg_html');
+      table.renameColumn('subject', 'title');
+      table.string('location');
+      table.string('date');
+      table.enu('type', ['msg', 'event']);
+    }),
+  ]);
+};
