e089dd35e5bb48d6eebbe7d7e6a62a6aca4733ed
Sim <thenambawan@gmail.com>
2018-08-25 15:26:30 +0200

    added basic filter for activities

---
+++ src/components/ActivityFilter/ActivityFilter.js
@@ -0,0 +1,127 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './ActivityFilter.css';
+import Box from '../Box';
+import CheckBox from '../CheckBox';
+import Label from '../Label';
+import SearchField from '../SearchField';
+import Button from '../Button';
+import { ICONS } from '../../constants';
+
+class ActivityFilter extends React.Component {
+  static propTypes = {
+    values: PropTypes.shape({}).isRequired,
+    onSelect: PropTypes.func.isRequired,
+    fetchUser: PropTypes.func.isRequired,
+    userData: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  };
+
+  static defaultProps = {};
+
+  constructor(props) {
+    super(props);
+    this.handleReset = this.handleReset.bind(this);
+  }
+
+  handleReset(field) {
+    const { values, onSelect } = this.props;
+
+    onSelect({ type: field, value: values[field] });
+    if (field === 'actorId') {
+      this.searchField.reset();
+    }
+  }
+
+  renderResetButton(field) {
+    let btn;
+    const { values } = this.props;
+    if (values[field]) {
+      btn = (
+        <Button
+          plain
+          onClick={() => this.handleReset(field)}
+          icon={
+            <svg
+              version="1.1"
+              viewBox="0 0 24 24"
+              width="24px"
+              height="24px"
+              role="img"
+              aria-label="close"
+            >
+              <path
+                fill="none"
+                stroke="#000"
+                strokeWidth="2"
+                d={ICONS.delete}
+              />
+            </svg>
+          }
+        />
+      );
+    }
+    return btn;
+  }
+
+  render() {
+    const { fetchUser, userData, values, onSelect } = this.props;
+    return (
+      <Box wrap>
+        <Box className={s.container} column>
+          <span>
+            <Label>Resource</Label>
+            {this.renderResetButton('type')}
+          </span>
+          {[
+            'USER',
+            'COMMENT',
+            'MESSAGE',
+            'PROPOSAL',
+            'LIKE',
+            'VOTE',
+            'REQUEST',
+            'STATEMENT',
+            'DISCUSSION',
+          ].map(type => (
+            <CheckBox
+              checked={values.type === type}
+              label={type}
+              onChange={() => onSelect({ type: 'type', value: type })}
+            />
+          ))}
+        </Box>
+        <Box column className={s.container}>
+          <span>
+            <Label>Action</Label>
+            {this.renderResetButton('verb')}
+          </span>
+          {['CREATE', 'UPDATE', 'DELETE'].map(verb => (
+            <CheckBox
+              checked={values.verb === verb}
+              label={verb.toUpperCase()}
+              onChange={() => onSelect({ type: 'verb', value: verb })}
+            />
+          ))}
+        </Box>
+        <Box column className={s.container}>
+          <span>
+            <Label>Actor</Label>
+            {this.renderResetButton('actorId')}
+          </span>
+          <SearchField
+            // eslint-disable-next-line no-return-assign
+            onRef={elm => (this.searchField = elm)}
+            data={userData}
+            fetch={fetchUser}
+            displaySelected={data =>
+              onSelect({ type: 'actorId', value: data.id })
+            }
+          />
+        </Box>
+      </Box>
+    );
+  }
+}
+
+export default withStyles(s)(ActivityFilter);
