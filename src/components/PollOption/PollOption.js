6a594439e0d41e6ca662f7135b155536d8a2c9e9
nambawan <thenambawan@gmail.com>
2018-10-11 17:26:19 +0200

    implemented followee-votes and voter on poll options, redesigned it a bit, added filter and collapse functionality

--- src/components/PollOption/PollOption.js
+++ src/components/PollOption/PollOption.js
@@ -4,23 +4,43 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './PollOption.css';
 import Box from '../Box';
 import CheckBox from '../CheckBox';
+import Meter from '../Meter';
+import Value from '../Value';
+import Card from '../Card';
+import UserThumbnail from '../UserThumbnail';
+import VotesList from '../VotesList';
 
 type PollOptionProps = {
   description: string,
-  onChange: () => void,
+  onChange: ({ pos: number }) => void,
   checked: boolean,
   disabled: boolean,
   numVotes: number,
   pos: number,
+  showVotes: boolean,
+  allVotes: number,
+  isCollapsed: boolean,
+  onLoadVotes: () => void,
+  followeeVotes: VoteShape[],
+  votes: VoteShape[],
+  updates: UpdatesShape,
 };
 
-class PollOption extends React.Component<PollOptionProps> {
+type State = {
+  votesExpanded?: boolean,
+};
+
+class PollOption extends React.Component<PollOptionProps, State> {
   constructor(props) {
     super(props);
+    this.state = {};
     this.handleChange = this.handleChange.bind(this);
+    this.handleLoadVotes = this.handleLoadVotes.bind(this);
   }
 
   handleChange: () => void;
+
+  handleLoadVotes: () => void;
 
   handleChange() {
     const { pos, onChange } = this.props;
@@ -29,18 +49,78 @@ class PollOption extends React.Component<PollOptionProps> {
     }
   }
 
+  handleLoadVotes() {
+    const { onLoadVotes } = this.props;
+    if (onLoadVotes) {
+      this.setState(
+        ({ votesExpanded }) => ({ votesExpanded: !votesExpanded }),
+        () => this.state.votesExpanded && onLoadVotes(), // eslint-disable-line
+      );
+    }
+  }
+
   render() {
-    const { description, checked, disabled, numVotes } = this.props;
+    const {
+      description,
+      checked,
+      disabled,
+      numVotes,
+      showVotes,
+      allVotes,
+      isCollapsed,
+      followeeVotes,
+      votes,
+      onLoadVotes,
+      updates,
+    } = this.props;
+
+    const { votesExpanded } = this.state;
+    let text = description;
+    if (isCollapsed && text.length > 27) {
+      text = `${text.slice(0, 30)} ...`;
+    }
+
     return (
-      <Box>
-        <CheckBox
-          checked={checked}
-          disabled={disabled}
-          onChange={this.handleChange}
-          label={description}
-        />
-        NumVotes: {numVotes}
-      </Box>
+      <Card className={s.root}>
+        <Box column>
+          <Box between align>
+            <Box align>
+              <CheckBox
+                checked={checked}
+                disabled={disabled}
+                onChange={this.handleChange}
+              />
+              <span>{text}</span>
+            </Box>
+            <Value value={numVotes} />
+          </Box>
+          {showVotes && (
+            <Box column onClick={this.handleLoadVotes}>
+              <Meter
+                className={s.meter}
+                trailWidth={3}
+                trailColor="#eee"
+                strokeColor={checked ? '#8cc800' : '#e0d500'}
+                strokeWidth={5}
+                percent={allVotes ? (numVotes * 100) / allVotes : 0}
+              />
+              {votesExpanded && (
+                <VotesList
+                  unipolar
+                  votes={votes}
+                  getVotes={onLoadVotes}
+                  autoLoadVotes
+                  isFetching={updates.pending}
+                  errorMessage={updates.error}
+                />
+              )}
+            </Box>
+          )}
+          {followeeVotes.map(v => (
+            <UserThumbnail user={v.voter} />
+          ))}
+        </Box>
+      </Card>
     );
   }
 }
