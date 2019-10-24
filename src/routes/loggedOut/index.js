ab1a1a6c6781fdde61a41e152e424cf17521b12c
nambawan <thenambawan@gmail.com>
2019-10-25 00:35:17 +0200

    localized logout-page

--- src/routes/loggedOut/index.js
+++ src/routes/loggedOut/index.js
@@ -1,37 +1,53 @@
 import React from 'react';
+import { FormattedMessage, defineMessages } from 'react-intl';
 import Layout from '../../components/Layout';
 import Login from '../../components/Login';
+import Heading from '../../components/Heading';
+import Box from '../../components/Box';
+import Label from '../../components/Label/Label';
+import Button from '../../components/Button/Button';
+import history from '../../history';
 
+const messages = defineMessages({
+  out: {
+    id: 'notice.loggedOut',
+    defaultMessage: 'You are logged out.',
+    description: 'notice when user is logged out',
+  },
+  thanks: {
+    id: 'labels.thankyou',
+    defaultMessage: 'Thank you for the visit!',
+    description: 'notice when sees logged out page',
+  },
+  start: {
+    id: 'labels.startpage',
+    defaultMessage: 'Start Page',
+    description: 'label of start page',
+  },
+});
 async function action() {
   return {
     chunks: ['loggedOut'],
     title: 'Logged out',
     component: (
       <Layout>
-        <div
-          style={{
-            paddingLeft: '20px',
-            paddingRight: '20px',
-            paddingTop: '20px',
-          }}
-        >
-          <div
-            style={{
-              padding: '0 0 40px',
-              maxWidth: '380px',
-            }}
-          >
-            <div style={{ marginBottom: '3em' }}>
-              <span style={{ fontSize: '1.5em' }}>LOGGED OUT </span>
+        <Box fill justify>
+          <Box column>
+            <Box column align>
+              <Heading tag="h2">
+                <FormattedMessage {...messages.out} />
+              </Heading>
               <br />
-              <span style={{ fontSize: '1em' }}>Thank you for the visit </span>
-
-              <br />
-              <span style={{ fontSize: '1em' }}>Come back? </span>
-            </div>
+              <Label>
+                <FormattedMessage {...messages.thanks} />
+              </Label>
+              <Button accent onClick={() => history.push('/')}>
+                <FormattedMessage {...messages.start} />
+              </Button>
+            </Box>
             <Login />
-          </div>
-        </div>
+          </Box>
+        </Box>
       </Layout>
     ),
   };
